b355db40281c7d3023ec25443cd9ecec5cc7eced
Jim Riddle <jimriddle0@gmail.com>
2022-04-21 19:12:21 -0500

    iteration 1 working

--- night_writer.rb
+++ night_writer.rb
@@ -1,8 +1,25 @@
-if ARGV.length < 2
-  puts "Too few arguments"
+# require 'file'
+
+if ARGV.length != 2
+  puts 'wrong number of arguments'
   exit
 end
 
-require 'pry'; binding.pry
 
-puts "Working on #{ARGV}"
+
+file_name_1 = ARGV[0]
+file_name_2 = ARGV[1]
+
+message = File.open(file_name_1, "r")
+# file = File.open(file_name_1)
+# File.open(<filename>, "w")
+
+# puts "Working on #{ARGV}"
+
+text_array = message.readlines
+character_count = text_array.join.length - 1
+
+
+puts "Created '#{ARGV[1]}' containing #{character_count} characters"
+
+require 'pry'; binding.pry
