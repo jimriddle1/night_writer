e51e96e5b67a482250dadbffd4fb6d51146df386
Jim Riddle <jimriddle0@gmail.com>
2022-04-21 20:04:43 -0500

    wrote test for writing the letter r

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -6,8 +6,18 @@ require './lib/dictionary'
 RSpec.describe Dictionary do
 
   it 'exists' do
-    dictionary = Dictionary.new
+    dictionary = Dictionary.new("Braille")
     expect(dictionary).to be_an_instance_of(Dictionary)
+  end
+
+  it 'has readable attrs' do
+    dictionary = Dictionary.new("Braille")
+    expect(dictionary.name).to eq("Braille")
+  end
+
+  it 'can create an r' do
+    dictionary = Dictionary.new("Braille")
+    expect(dictionary.create_r).to eq(".0\n..\n.0")
   end
 
 end
